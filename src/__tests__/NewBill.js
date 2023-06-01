2c705a0841174e3484d77de4f79757e0001e80ad
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-06-02 00:36:03 +0200

    Ensemble des tests unitaires pour NewBill.js

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -4,7 +4,7 @@
 
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import { screen } from "@testing-library/dom";
+import { screen, fireEvent } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { ROUTES } from "../constants/routes.js";
@@ -32,10 +32,7 @@ describe("Given I am connected as an employee", () => {
     test("Then I can upload a file with extension jpg, jpeg, or png", () => {
       document.body.innerHTML = NewBillUI();
       const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage,
+        document, onNavigate, store: mockStore, localStorage: window.localStorage,
       });
 
       const input = screen.getByTestId("file");
@@ -48,6 +45,55 @@ describe("Given I am connected as an employee", () => {
       expect(handleChangeFile).toHaveBeenCalled();
       expect(input.files[0]).toStrictEqual(file);
       expect(input.files[0].name).toBe("image.jpg");
+    });
+
+    test("Then I can't upload a file with extension different than jpg, jpeg, or png", () => {
+      document.body.innerHTML = NewBillUI();
+      const newBill = new NewBill({
+        document, onNavigate, store: null, localStorage: window.localStorage,
+      });
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const input = screen.getByTestId("file");
+      input.addEventListener("change", handleChangeFile);
+      userEvent.upload(input, new File(["file"], "file.txt", { type: "text/plain" }));
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(input.value).toBe("");
+    });
+
+    test("Then I can post a new bill", () => {
+      document.body.innerHTML = NewBillUI;
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+      const form = screen.getByTestId("form-new-bill");
+
+      // inputs
+      const datepicker = screen.getByTestId("datepicker");
+      userEvent.type(datepicker, "2022-05-02");
+
+      const amount = screen.getByTestId("amount");
+      userEvent.type(amount, "205");
+
+      const tva = screen.getByTestId("pct");
+      userEvent.type(tva, "20");
+
+      const file = screen.getByTestId("file");
+      userEvent.upload(
+        file,
+        new File(["image"], "image.jpg", { type: "image/jpg" })
+      );
+
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(form).toBeTruthy();
     });
   });
 });
