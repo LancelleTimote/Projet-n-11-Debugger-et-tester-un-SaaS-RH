d49723d2dc2743a2312b0de3e0055df6aae51ea4
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-06-02 10:10:35 +0200

    Premier aperçu tests unitaires bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -8,6 +8,9 @@ import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
+import mockStore from "../__mocks__/store";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -37,5 +40,79 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+
+    //Test GET
+    test("fetches bills from mock API GET", () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      //Mock bills
+      const bills = new Bills({
+        document, onNavigate, localStorage, store: mockStore,
+      });
+      bills.getBills((data) => {
+        root.innerHTML = BillsUI({ data });
+        expect(document.querySelector("tbody").rows.length).toBeGreaterThan(0);
+      });
+    });
+  });
+
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      const html = BillsUI({ error: "Erreur 404" });
+      document.body.innerHTML = html;
+      const message = await screen.getAllByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+    test("fetches message from an API abd fails with 500 message error", async () => {
+      const html = BillsUI({ error: "Erreur 500" });
+      document.body.innerHTML = html;
+      const message = await screen.getAllByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    });
+  });
+  //Fin test GET
+
+  test("Then when I click on the icon eye it should open the modal", () => {
+    document.body.innerHTML = BillsUI({
+      data: bills,
+    });
+
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+    const billsList = new Bills({
+      document, onNavigate, store: null, localStorage: window.localStorage,
+    });
+    $.fn.modal = jest.fn();
+    const icon = screen.getAllByTestId("icon-eye")[0];
+    const handleClickIconEye = jest.fn(() =>
+      billsList.handleClickIconEye(icon)
+    );
+    icon.addEventListener("click", handleClickIconEye);
+    userEvent.click(icon);
+    expect(handleClickIconEye).toHaveBeenCalled();
+    const modale = document.getElementById("modaleFile");
+    expect(modale).toBeTruthy();
   });
 });
